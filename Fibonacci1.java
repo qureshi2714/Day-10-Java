17db5dda7beea69fc55dd115f1526bd950721ac7
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-11 13:26:21 +0530

    Add files via upload

---
+++ Fibonacci1.java
@@ -0,0 +1,15 @@
+class Fibonacci1{
+    public static void main(String args[])
+    {
+        int a=0;
+        int b=1;
+        for(int i=2;i<=10;i++)
+        {
+            int c=a+b;
+            System.out.println(i+"->"+c);
+            a=b;b=c;
+        }
+    }
+
+
+}

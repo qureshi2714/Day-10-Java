875f83d8c4c14cdc47c0acbd725cc464af5a3945
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-11 11:10:12 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,12 @@
+class Main1 {
+    static String s()
+    {
+        return "hello Students";
+    }
+    public static void main(String[] args){
+        String res=s();
+        System.out.println(res);
+    }
+
+    
+}

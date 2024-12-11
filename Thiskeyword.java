05b0608c9b61cb8c72f4e781d3d896b53ee2e749
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-11 14:20:37 +0530

    Add files via upload

---
+++ Thiskeyword.java
@@ -0,0 +1,27 @@
+
+        class a{
+            int c;
+            String b;
+            a()
+            {
+                this.c=345;
+                this.b="Hello All";
+            }
+            void display()
+            {
+                System.out.println(b+" "+c);
+            }
+        }
+        public class Thiskeyword{
+            public static void main(String args[]){
+                a s = new a();
+                s.display();
+            }
+        }
+
+    
+        
+
+    
+    
+

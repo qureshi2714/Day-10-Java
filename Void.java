ee5014892303ca1531cdfd5ff256a5583f5e5790
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-11 11:05:42 +0530

    Add files via upload

---
+++ Void.java
@@ -0,0 +1,12 @@
+class Void {
+    public static String s()
+    {
+        return "hello students";
+    }
+    public static void main(String args[]){
+        String res=s();
+        System.out.println(res);
+    }
+    
+    
+}

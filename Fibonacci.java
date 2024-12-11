6ac3f23ef47ae097915bc8fcaa6617132b761d74
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-11 13:25:58 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+class Fibonacci {
+    public static int fib(int n)
+    {
+        if(n==0 || n==1)
+        {
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String args[])
+    {
+        int res = fib(6);
+        System.out.println(res);
+    }
+
+}
